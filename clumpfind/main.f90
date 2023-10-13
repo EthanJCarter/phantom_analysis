3ae50535618cf0f11099ac40d9fd1f1d1349801a
Ethan Carter <ethan.j.carter@outlook.com>
2023-10-13 13:09:03 +0100

    main

---
+++ clumpfind/main.f90
@@ -0,0 +1,10 @@
+!main_file for clump_analysis files
+
+program main
+
+    use find_clumps, only: read_nclumps_and_nsinks, read_clumpdata
+    implicit none
+
+    call read_nclumps_and_nsinks('clumps_and_sinks002.dat')
+
+end program main
